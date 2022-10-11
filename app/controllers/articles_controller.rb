4d372c454dfc9eab6dc9e0398e84d842f3eca272
Gaagul Gigi <70290286+gaagul@users.noreply.github.com>
2022-10-11 22:49:18 +0530

    7 article category controllers (#13)

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+class ArticlesController < ApplicationController
+  before_action :load_article!, except: %i[index create]
+
+  def index
+    articles = Article.all.order(updated_at: :desc)
+    @all_articles = articles
+    @draft_articles = articles.where(status: :draft)
+    @published_articles = articles.where(status: :published)
+  end
+
+  def show
+    respond_with_json({ article: @article })
+  end
+
+  def create
+    article = Article.new(article_params)
+    article.save!
+    respond_with_success(t("successfully_created", entity: "Article"))
+  end
+
+  def update
+    @article.update!(article_params)
+    respond_with_success(t("successfully_updated", entity: "Article"))
+  end
+
+  def destroy
+    @article.destroy!
+    respond_with_success(t("successfully_deleted", entity: "article"))
+  end
+
+  private
+
+    def load_article!
+      @article = Article.find_by!(slug: params[:slug])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :author, :status, :category_id, :body)
+    end
+end
---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def create
+    Category.create!(category_params)
+    respond_with_success(t("successfully_created", entity: "Category"))
+  end
+
+  def update
+    @category.update!(category_params)
+    respond_with_success(t("successfully_updated", entity: "Category"))
+  end
+
+  def destroy
+    @category.destroy!
+    respond_with_success(t("successfully_deleted", entity: "Category"))
+  end
+
+  private
+
+    def category_params
+      params.require(:category).permit(:id, :title)
+    end
+
+    def load_category!
+      @category = Category.find_by!(id: params[:id])
+    end
+end
