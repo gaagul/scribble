a707ea5dee98a02886e085e4092278c64fb4d8bc
Gaagul Gigi <gaagulgigi@gmail.com>
2022-10-16 10:53:13 +0530

    added redirection migrations

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_14_154204) do
+ActiveRecord::Schema.define(version: 2022_10_16_044241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 2022_10_14_154204) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "redirections", force: :cascade do |t|
+    t.string "from"
+    t.string "to"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["from"], name: "index_redirections_on_from", unique: true
   end
 
   add_foreign_key "articles", "categories"
