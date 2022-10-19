65b815e79e3dc46fa8cdeaff064257384f873e57
Gaagul Gigi <gaagulgigi@gmail.com>
2022-10-19 11:27:38 +0530

    fixed form validations

--- app/javascript/src/components/Dashboard/Articles/Pane/constants.js
+++ app/javascript/src/components/Dashboard/Articles/Pane/constants.js
@@ -8,5 +8,6 @@ export const CREATE_FORM_INITIAL_VALUES = {
 
 export const ARTICLE_FORM_VALIDATION_SCHEMA = Yup.object().shape({
   title: Yup.string().max(30).required("Title is required"),
+  category: Yup.object().required("Select a Category"),
   body: Yup.string().required("Body of the article cannot be empty"),
 });
