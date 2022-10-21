b6eb302956f51c47d078fa0d5d5c825c366a9992
Gaagul Gigi <gaagulgigi@gmail.com>
2022-10-21 19:12:44 +0530

    fixed edit bugs

--- app/javascript/src/components/Dashboard/Articles/Options/Edit.jsx
+++ app/javascript/src/components/Dashboard/Articles/Options/Edit.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 
-import { useParams } from "react-router-dom";
+import { PageLoader } from "neetoui";
+import { isNil, isEmpty, either } from "ramda";
+import { useHistory, useParams } from "react-router-dom";
 
 import articlesApi from "apis/articles";
 
@@ -8,7 +10,9 @@ import Form from "./Form";
 
 const Edit = () => {
   const [article, setArticle] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { slug } = useParams();
+  const history = useHistory();
 
   const fetchArticle = async () => {
     try {
@@ -18,6 +22,8 @@ const Edit = () => {
       setArticle(article);
     } catch (error) {
       logger.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -25,6 +31,22 @@ const Edit = () => {
     fetchArticle();
   }, []);
 
-  return <Form isEdit article={article} />;
+  if (loading) {
+    return (
+      <div className="h-screen w-screen">
+        <PageLoader />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      {!either(isNil, isEmpty)(article) ? (
+        <Form isEdit article={article} />
+      ) : (
+        history.push("/")
+      )}
+    </>
+  );
 };
 export default Edit;
