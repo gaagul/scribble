99f7e06988949436878a54c74010c008e5ec161e
Gaagul Gigi <gaagulgigi@gmail.com>
2022-10-23 20:47:50 +0530

    setup rake tasks and temp slug for draft

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,23 +14,36 @@ class Article < ApplicationRecord
   private
 
     def set_slug
-      status == "Published" ?
-         (title_slug = title.parameterize
-          latest_task_slug = Article.where(
-            "slug ~* ?",
-            "#{title_slug}$|#{title_slug}-[0-9]+$",
-          ).order("LENGTH(slug) DESC", slug: :desc).first&.slug
-          slug_count = 0
-          if latest_task_slug.present?
-            slug_count = latest_task_slug.split("-").last.to_i
-            only_one_slug_exists = slug_count == 0
-            slug_count = 1 if only_one_slug_exists
-          end
-          slug_candidate = slug_count.positive? ? "#{title_slug}-#{slug_count + 1}" : title_slug
-          self.slug = slug_candidate)
-       :
-       self.slug = ""
-    end
+      if status == "Published"
+        title_slug = title.parameterize
+        latest_task_slug = Article.where(
+          "slug ~* ?",
+          "#{title_slug}$|#{title_slug}-[0-9]+$",
+        ).order("LENGTH(slug) DESC", slug: :desc).first&.slug
+        slug_count = 0
+        if latest_task_slug.present?
+          slug_count = latest_task_slug.split("-").last.to_i
+          only_one_slug_exists = slug_count == 0
+          slug_count = 1 if only_one_slug_exists
+        end
+        slug_candidate = slug_count.positive? ? "#{title_slug}-#{slug_count + 1}" : title_slug
+        self.slug = slug_candidate
+      else
+        title_slug = title.parameterize + "-temp"
+        latest_temp_slug = Article.where(
+          "slug ~* ?",
+          "#{title_slug}$|#{title_slug}-[0-9]+$",
+        ).order("LENGTH(slug) DESC", slug: :desc).first&.slug
+        slug_count = 0
+        if latest_task_slug.present?
+          slug_count = latest_task_slug.split("-").last.to_i
+          only_one_slug_exists = slug_count == 0
+          slug_count = 1 if only_one_slug_exists
+        end
+        slug_candidate = slug_count.positive? ? "#{title_slug}-#{slug_count + 1}" : title_slug
+        self.slug = slug_candidate
+      end
+   end
 
     def slug_not_changed
       if slug_changed? && self.persisted?
