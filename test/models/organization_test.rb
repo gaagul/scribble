bf6659f2e4714c2098dd0b0df9e6be6a984c2ed0
Gaagul Gigi <gaagulgigi@gmail.com>
2022-10-24 11:32:35 +0530

    user and organization tests

---
+++ test/models/organization_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class OrganizationTest < ActiveSupport::TestCase
+  def setup
+    @organization = build(:organization)
+  end
+
+  def test_organization_should_not_be_valid_and_saved_without_title
+    @organization.title = ""
+    assert_not @organization.valid?
+    assert_includes @organization.errors.full_messages, "Title can't be blank"
+  end
+
+  def test_title_should_be_of_valid_length
+    @organization.title = "a" * (Organization::MAX_NAME_LENGTH + 1)
+    assert @organization.invalid?
+  end
+
+  def test_organizations_should_have_unique_auth_token
+    @organization.save!
+    second_organization = build(:organization)
+    assert_not_same @organization.authentication_token, second_organization.authentication_token
+  end
+end
