10fddc0b171132963146bed62fd61e2eb352cddc
Gaagul Gigi <gaagulgigi@gmail.com>
2022-10-24 23:47:09 +0530

    added without title test for articles controller

--- app/javascript/src/components/Dashboard/Articles/Options/constants.js
+++ app/javascript/src/components/Dashboard/Articles/Options/constants.js
@@ -8,7 +8,10 @@ export const CREATE_FORM_INITIAL_VALUES = {
 };
 
 export const ARTICLE_FORM_VALIDATION_SCHEMA = Yup.object().shape({
-  title: Yup.string().max(30).required("Title is required"),
+  title: Yup.string()
+    .max(30)
+    .matches(/([A-z0-9])/, "Is not in correct format")
+    .required("Title is required"),
   category: Yup.object().nullable().required("Select a Category"),
   body: Yup.string().required("Body of the article cannot be empty"),
 });
--- app/models/article.rb
+++ app/models/article.rb
@@ -9,7 +9,7 @@ class Article < ApplicationRecord
   enum status: { Draft: 0, Published: 1 }
 
   validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }, format: { with: /([A-z0-9])/ }
-  validates :slug, uniqueness: true
+  validates :slug, uniqueness: true unless -> { slug.nil? }
   validate :slug_not_changed
 
   before_save :set_slug
@@ -17,19 +17,21 @@ class Article < ApplicationRecord
   private
 
     def set_slug
-      title_slug = title.parameterize
-      latest_task_slug = Article.where(
-        "slug ~* ?",
-        "#{title_slug}$|#{title_slug}-[0-9]+$",
-      ).order("LENGTH(slug) DESC", slug: :desc).first&.slug
-      slug_count = 0
-      if latest_task_slug.present?
-        slug_count = latest_task_slug.split("-").last.to_i
-        only_one_slug_exists = slug_count == 0
-        slug_count = 1 if only_one_slug_exists
+      if status == "Published" && slug.nil?
+        title_slug = title.parameterize
+        latest_task_slug = Article.where(
+          "slug ~* ?",
+          "#{title_slug}$|#{title_slug}-[0-9]+$",
+        ).order("LENGTH(slug) DESC", slug: :desc).first&.slug
+        slug_count = 0
+        if latest_task_slug.present?
+          slug_count = latest_task_slug.split("-").last.to_i
+          only_one_slug_exists = slug_count == 0
+          slug_count = 1 if only_one_slug_exists
+        end
+        slug_candidate = slug_count.positive? ? "#{title_slug}-#{slug_count + 1}" : title_slug
+        self.slug = slug_candidate
       end
-      slug_candidate = slug_count.positive? ? "#{title_slug}-#{slug_count + 1}" : title_slug
-      self.slug = slug_candidate
     end
 
     def slug_not_changed
