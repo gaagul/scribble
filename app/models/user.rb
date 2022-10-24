aa49e1739439b3752ad0e2ee2099d7dbd9338b90
Gaagul Gigi <gaagulgigi@gmail.com>
2022-10-24 10:35:38 +0530

    Added tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  MAX_NAME_LENGTH = 35
+
   has_many :articles, dependent: :delete_all
 
-  validates :name, presence: true, length: { maximum: 35 }
+  validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
 end
