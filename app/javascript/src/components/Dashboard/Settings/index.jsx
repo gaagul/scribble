8035c88b6d140a532b9163d97e2f418430d9ab69
Gaagul Gigi <70290286+gaagul@users.noreply.github.com>
2022-10-25 10:07:46 +0530

    Settings Routes to query string (#92)

--- app/javascript/src/components/Dashboard/Settings/index.jsx
+++ app/javascript/src/components/Dashboard/Settings/index.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { Switch, Route, useRouteMatch, Redirect } from "react-router-dom";
+import { MenuBar } from "neetoui/layouts";
+import queryString from "query-string";
 
-import Categories from "./Categories";
-import General from "./General";
-import Redirections from "./Redirections";
-import SideMenuBar from "./SideMenuBar";
+import { SETTINGS_NAVLINKS } from "./constants";
+import { getActiveNavLink } from "./utils";
 
-const Settings = () => {
-  const { path, url } = useRouteMatch();
+const Settings = ({ history, location }) => {
+  const { tab } = queryString.parse(location.search);
+  const [activeNavlink, setActiveNavlink] = useState(
+    () => getActiveNavLink(tab) || SETTINGS_NAVLINKS[0]
+  );
+  useEffect(() => history.push(activeNavlink?.path), [activeNavlink, history]);
+
+  if (location.state?.resetTab) {
+    location.state.resetTab = null;
+    setActiveNavlink(() => getActiveNavLink(tab));
+  }
 
   return (
     <div className="flex ">
-      <SideMenuBar url={url} />
-      <Switch>
-        <Route exact component={General} path={`${path}/general`} />
-        <Route exact component={Categories} path={`${path}/categories`} />
-        <Route exact component={Redirections} path={`${path}/redirection`} />
-        <Redirect exact from={`${path}`} to={`${path}/general`} />
-      </Switch>
+      <MenuBar showMenu title="Settings">
+        {SETTINGS_NAVLINKS.map(navlink => (
+          <div className="flex items-center" key={navlink.key}>
+            <navlink.icon className="mb-2" size={30} />
+            <MenuBar.Item
+              active={tab === navlink.key}
+              description={navlink.description}
+              key={navlink.key}
+              label={navlink.label}
+              onClick={() => setActiveNavlink(navlink)}
+            />
+          </div>
+        ))}
+      </MenuBar>
+      <activeNavlink.component />
     </div>
   );
 };
