97ec496da71208bb520c7776cb2df53dbb951926
Gaagul Gigi <70290286+gaagul@users.noreply.github.com>
2022-10-25 09:22:53 +0530

    articles controller tests (#90)

---
+++ test/controllers/articles_controller_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class ArticlesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = create(:user)
+    @category = create(:category)
+    @organization = create(:organization)
+    @article = create(:article, category: @category, user: @user, organization: @organization)
+  end
+
+  def test_shouldnt_create_article_without_title
+    post articles_path, params: {
+                          article:
+                          {
+                            title: "", body: "Learn Ruby",
+                            user: @user, category_id: @category.id, status: "Published"
+                          }
+
+                        },
+      headers: headers
+    assert_response :unprocessable_entity
+    response_json = response.parsed_body
+    assert_equal response_json["error"], "Title can't be blank and Title is invalid"
+  end
+end
