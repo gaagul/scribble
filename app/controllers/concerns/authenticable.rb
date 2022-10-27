935038caa41f0cb8c73ee1595e5dd164417eac59
Gaagul Gigi <gaagulgigi@gmail.com>
2022-10-27 15:44:04 +0530

    enabled backent auth using token

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -10,15 +10,10 @@ module Authenticable
   private
 
     def authenticate_user_using_x_auth_token
-      user_email = request.headers["X-Auth-Email"].presence
-      auth_token = request.headers["X-Auth-Token"].to_s
-      user = user_email && User.find_by!(email: user_email)
-      is_valid_token = auth_token && ActiveSupport::SecurityUtils.secure_compare(
-        user.authentication_token,
-        auth_token)
-      if is_valid_token
-        @current_user = user
-      else
+      auth_token = request.headers["X-Auth-Token"].presence
+      is_valid_token = current_organization && auth_token && ActiveSupport::SecurityUtils.secure_compare(
+        current_organization.authentication_token, auth_token)
+      unless !current_organization.is_password_enabled? || is_valid_token
         respond_with_error(t("session.could_not_auth"), :unauthorized)
       end
     end
